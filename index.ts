5df295550defb9dc7ce6c2793434aa6c8fa21d8c
Allan Gaiteiro <64567248+AllanGaiteiro@users.noreply.github.com>
2023-04-06 21:08:11 -0300

    refatoração do index.ts

--- index.ts
+++ index.ts
@@ -1,17 +1,8 @@
-import generateCode from "./src/gpt";
+import App from "./src/app";
+import appController from "./src/app.controller";
 
-const express = require('express');
-
-const app = express();
-app.use(express.json());
-
-app.get("/", async (req: { body: { question: any; }; }, res: { send: (arg0: string) => void; }) => {
-    const { question } = req.body;
-    const code = await generateCode(question);
-    res.send(code ?? '');
-});
-
-const PORT = 4000;
-app.listen(PORT, () => {
-    console.log(`App listening at http://localhost:${PORT}`);
-});
+function iniciar() {
+    const app = new App([appController], 4000)
+    app.listen();
+}
+iniciar();
